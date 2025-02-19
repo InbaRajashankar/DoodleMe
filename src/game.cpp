44e4c2ec64e6e7724dc9c4982ad909ef78b3320c
Inba <inbarajashankar@gmail.com>
2025-02-19 20:09:05 +0100

    Added file-reading

--- src/game.cpp
+++ src/game.cpp
@@ -1,19 +1,86 @@
 #include <iostream>
 #include <thread>
-#include <chrono>
+#include <chrono> // time
+
+#include <fstream>
+#include <vector>
+
+#include <sstream>
 
 #include "game.h"
 
-/*
+std::vector<std::string> skeletons, adjectives, nounsSng, nounsPlu;
+
+/**
+ * @brief Reads each line of a file as a string, which are pushed into a vector.
+ */
+void Game::file_to_vector(const std::string& path, std::vector<std::string>& vec)
+{
+  std::string buffer;
+  std::ifstream curFile (path);
+
+  if (!curFile.is_open())
+  {
+    std::cout << "Unable to open " << path << "!\n";
+    exit(0);
+  }
+
+  while (std::getline (curFile, buffer))
+    vec.push_back(buffer);
+
+  curFile.close();
+}
+
+/**
+ * @brief Reads each line of file as a comma-separated string, which are pushed into two vectors.
+ */
+void Game::file_to_two_vector(const std::string& path, std::vector<std::string>& vec1, std::vector<std::string>& vec2)
+{
+  std::string buffer;
+  std::string word1;
+  std::string word2;
+
+  std::ifstream curFile (path);
+
+  if (!curFile.is_open())
+  {
+    std::cout << "Unable to open " << path << "!\n";
+    exit(0);
+  }
+
+  while (std::getline (curFile, buffer))
+  {
+    std::stringstream ss(buffer);
+    while (ss.good())
+    {
+      std::getline(ss, word1, ',');
+      std::getline(ss, word2);
+      vec1.push_back(word1);
+      vec2.push_back(word2);
+    }
+  }
+  
+  curFile.close();
+}
+
+/**
  * @brief Constructor for a game object. 
  */
 Game::Game()
 {
-  std::cout << "Hello, the game will start now!\n";
+  Game::process_files();
+  std::cout << "Hello, the game will start now\n";
   Game::start();
 }
 
-/*
+void Game::process_files()
+{
+  Game::file_to_vector("src/text_files/adjectives.txt", adjectives);
+  Game::file_to_two_vector("src/text_files/nouns.txt", nounsSng, nounsPlu);
+  Game::file_to_vector("src/text_files/skeletons.txt", skeletons);
+}
+
+/**
  * @brief Main gameflow logic. 
  */
 void Game::start()
@@ -29,7 +96,9 @@ void Game::start()
 
   while (play_more)
   {
-    std::cout << "\nDRAWING PROMPT\n";
+    std::cout << "\nDRAWING PROMPT\nPress enter to start the timer.";
+    std::getline(std::cin, buffer);
+    std::cout << "Timer started!\n";
     std::this_thread::sleep_for(std::chrono::seconds(wait));
     std::cout << "Time up!\nEnter q to stop the game, or anything else to continue:";
     std::getline(std::cin, buffer);
--- src/game.h
+++ src/game.h
@@ -2,11 +2,21 @@
 #define GAME_H
 #endif
 
+#include <vector>
+
 class Game
 {
 private:
+  std::vector<std::string> skeletons;
+  std::vector<std::string> adjectives;
+  std::vector<std::string> nounsSng;
+  std::vector<std::string> nounsPlu;
+
+  void file_to_vector(const std::string& path, std::vector<std::string>& vec);
+  void file_to_two_vector(const std::string& path, std::vector<std::string>& vec1, std::vector<std::string>& vec2);
 
 public:
   Game();
+  void process_files();
   void start();
 };
